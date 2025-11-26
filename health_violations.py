20eb496454d4047dbbf144cbe5bd521f612a5389
GabrielAlenksUSP <gabriel.antonio@alumni.usp.br>
2025-11-25 21:50:50 -0300

    Update health_violations.py

--- health_violations.py
+++ health_violations.py
@@ -1,50 +1,51 @@
-import argparse
+# import argparse
 
-from pyspark.sql import SparkSession
+# from pyspark.sql import SparkSession
 
-def calculate_red_violations(data_source, output_uri):
-    """
-    Processes sample food establishment inspection data and queries the data to find the top 10 establishments
-    with the most Red violations from 2006 to 2020.
+# def calculate_red_violations(data_source, output_uri):
+#     """
+#     Processes sample food establishment inspection data and queries the data to find the top 10 establishments
+#     with the most Red violations from 2006 to 2020.
 
-    :param data_source: The URI of your food establishment data CSV, such as 's3://DOC-EXAMPLE-BUCKET/food-establishment-data.csv'.
-    :param output_uri: The URI where output is written, such as 's3://DOC-EXAMPLE-BUCKET/restaurant_violation_results'.
-    """
-    with SparkSession.builder.appName("Calculate Red Health Violations").getOrCreate() as spark:
-        # Load the restaurant violation CSV data
-        if data_source is not None:
-            restaurants_df = spark.read.option("header", "true").csv(data_source)
+#     :param data_source: The URI of your food establishment data CSV, such as 's3://DOC-EXAMPLE-BUCKET/food-establishment-data.csv'.
+#     :param output_uri: The URI where output is written, such as 's3://DOC-EXAMPLE-BUCKET/restaurant_violation_results'.
+#     """
+#     with SparkSession.builder.appName("Calculate Red Health Violations").getOrCreate() as spark:
+#         # Load the restaurant violation CSV data
+#         if data_source is not None:
+#             restaurants_df = spark.read.option("header", "true").csv(data_source)
 
-        # Create an in-memory DataFrame to query
-        restaurants_df.createOrReplaceTempView("restaurant_violations")
+#         # Create an in-memory DataFrame to query
+#         restaurants_df.createOrReplaceTempView("restaurant_violations")
 
-        # Create a DataFrame of the top 10 restaurants with the most Red violations
-        top_red_violation_restaurants = spark.sql("""SELECT upper(Name) as name, count(*) AS total_red_violations 
-          FROM restaurant_violations 
-          WHERE `Violation Type` = 'RED' 
-          GROUP BY upper(Name) 
-          ORDER BY total_red_violations DESC LIMIT 10""")
+#         # Create a DataFrame of the top 10 restaurants with the most Red violations
+#         top_red_violation_restaurants = spark.sql("""SELECT upper(Name) as name, count(*) AS total_red_violations 
+#           FROM restaurant_violations 
+#           WHERE `Violation Type` = 'RED' 
+#           GROUP BY upper(Name) 
+#           ORDER BY total_red_violations DESC LIMIT 10""")
 
-        # Write the results to the specified output URI
-        top_red_violation_restaurants.write.option("header", "true").mode("overwrite").csv(output_uri + "Restaurants/")
+#         # Write the results to the specified output URI
+#         top_red_violation_restaurants.write.option("header", "true").mode("overwrite").csv(output_uri + "Restaurants/")
         
-        # Create a DataFrame of the top 10 cities with the most Red violations
-        top_red_violation_cities = spark.sql("""SELECT upper(City) as city, count(*) AS total_red_violations 
-          FROM restaurant_violations 
-          WHERE `Violation Type` = 'RED' 
-          GROUP BY upper(City) 
-          ORDER BY total_red_violations DESC LIMIT 10""")
+#         # Create a DataFrame of the top 10 cities with the most Red violations
+#         top_red_violation_cities = spark.sql("""SELECT upper(City) as city, count(*) AS total_red_violations 
+#           FROM restaurant_violations 
+#           WHERE `Violation Type` = 'RED' 
+#           GROUP BY upper(City) 
+#           ORDER BY total_red_violations DESC LIMIT 10""")
 
-        # Write the results to the specified output URI
-        top_red_violation_cities.write.option("header", "true").mode("overwrite").csv(output_uri + "Cities/")
+#         # Write the results to the specified output URI
+#         top_red_violation_cities.write.option("header", "true").mode("overwrite").csv(output_uri + "Cities/")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--data_source', help="The URI for you CSV restaurant data, like an S3 bucket location.")
-    parser.add_argument(
-        '--output_uri', help="The URI where output is saved, like an S3 bucket location.")
-    args = parser.parse_args()
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#         '--data_source', help="The URI for you CSV restaurant data, like an S3 bucket location.")
+#     parser.add_argument(
+#         '--output_uri', help="The URI where output is saved, like an S3 bucket location.")
+#     args = parser.parse_args()
     
-    calculate_red_violations(args.data_source, args.output_uri)
+#     calculate_red_violations(args.data_source, args.output_uri)
+
     			
